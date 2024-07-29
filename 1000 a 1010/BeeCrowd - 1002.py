c219f26cffa2d52a51a1058cfe4428179c03be3b
Ian Lima <ian.vslima@gmail.com>
2024-07-29 11:31:21 -0300

    Atualizacao
    Commit de três exercícios

---
+++ 1000 a 1010/BeeCrowd - 1002.py
@@ -0,0 +1,12 @@
+#raio = 3.14159
+#n = float(input())
+#area = raio * n**2
+
+#print(f"A={area:.4f}")
+
+import math
+raio = 3.14159
+n = float(input())
+area = raio * math.pow(n,2)
+
+print(f"A={area:.4f}")
