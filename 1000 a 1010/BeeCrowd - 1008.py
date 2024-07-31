f7410b1e6d2fe6f83fec81420952e189ea8efc3a
Ian Lima <ian.vslima@gmail.com>
2024-07-31 15:41:47 -0300

    Atualizacao 31-07

---
+++ 1000 a 1010/BeeCrowd - 1008.py
@@ -0,0 +1,8 @@
+id = int(input())
+horas = int(input())
+vl_hora = float(input())
+
+Salario = float(horas) * vl_hora
+
+input("NUMBER =",id)
+input("SALARY = U$", Salario)
---
+++ 1000 a 1010/BeeCrowd - 1009.py
@@ -0,0 +1,8 @@
+Nome = input()
+Salario = float(input())
+vendas = float(input())
+
+total = vendas * 0.15 + Salario
+
+
+print ("TOTAL = R$",total)
